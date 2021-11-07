5a6d0298dc6f5b638cd266bd587cb4cccdced546
Internet Person <b0x0r0cks@pm.me>
2021-11-07 16:28:43 -0500

    Chakra UI / Cleanup

--- components/DesignPending.tsx
+++ components/DesignPending.tsx
@@ -1,15 +1,22 @@
+import { Button } from "@chakra-ui/button";
+import { useColorMode } from "@chakra-ui/color-mode";
+import { Center, Text } from "@chakra-ui/layout";
+
+const ToggleColorModeButton = () => {
+  const { toggleColorMode } = useColorMode();
+
+  return (
+    <Button m={2} onClick={toggleColorMode}>
+      Toggle color mode
+    </Button>
+  );
+};
+
 export const DesignPendingBanner = () => {
   return (
-    <div
-      style={{
-        fontSize: 32,
-        backgroundColor: "red",
-        alignItems: "center",
-        textAlign: "center",
-        flex: 1,
-      }}
-    >
-      DESIGN PENDING...duh
-    </div>
+    <Center align="center" justify="center" bg="red.500" d="vertical">
+      <Text fontSize="3xl">DESIGN PENDING...duh</Text>
+      <ToggleColorModeButton />
+    </Center>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import React from "react";
 import { DesignPendingBanner } from "../components/DesignPending";
 import "../styles/globals.css";
+import theme from "../theming/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div>
-      <DesignPendingBanner />
-      <Component {...pageProps} />
-    </div>
+    <ChakraProvider theme={theme}>
+      <div>
+        <DesignPendingBanner />
+        <Component {...pageProps} />
+      </div>
+    </ChakraProvider>
   );
 }
 
