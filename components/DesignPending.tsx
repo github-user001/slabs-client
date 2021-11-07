9b10f45a217a12df7a3629cc8dfcbfb17c419940
Internet Person <b0x0r0cks@pm.me>
2021-11-07 15:31:33 -0500

    Netlify init

---
+++ components/DesignPending.tsx
@@ -0,0 +1,15 @@
+export const DesignPendingBanner = () => {
+  return (
+    <div
+      style={{
+        fontSize: 32,
+        backgroundColor: "red",
+        alignItems: "center",
+        textAlign: "center",
+        flex: 1,
+      }}
+    >
+      DESIGN PENDING...duh
+    </div>
+  );
+};
